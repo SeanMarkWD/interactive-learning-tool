d2b6225ff982346cbd81f21c87ad9f853de8646f
Sean Lenferna <seanlenferna@hotmail.com>
2024-04-06 19:11:35 +0300

    Remove README.md

--- README.md
+++
@@ -1,2 +0,0 @@
-# interactive-learning-tool
-Interactive learning tool for final project for Sprint 3 part 4
--- test_main.py
+++ test_main.py
@@ -1,4 +1,8 @@
-from main import Question
+import pytest
+import sys
+sys.path.append('/Users/sheepstone/InteractiveLearningTool/interactive-learning-tool')
+from main import Question, QuestionBank
+from unittest.mock import MagicMock
 
 
 def main():
@@ -6,27 +10,38 @@ def main():
     test_check_incorrect_answer()
     test_is_correct_case_insensitive()
     test_enable_disable_questions()
+    test_add_question()
 
-    def test_check_correct_answer():
-        question = Question("1", "What is the capital of France?", "Paris")
-        assert question.check_answer("Paris"), "Should return True for correct answer."
 
-    def test_check_incorrect_answer():
-        question = Question("2", "What is the capital of France?", "Paris")
-        assert not question.check_answer("Lyon"), "Should return False for incorrect answer."
+def test_check_correct_answer():
+    question = Question("1", "What is the capital of France?", "Paris")
+    assert question.check_answer("Paris"), "Should return True for correct answer."
 
-    def test_is_correct_case_insensitive():
-        question = Question("3", "What is the capital of France?", "Paris")
-        assert question.check_answer("paris"), "The method should return True for a case-insensitive match."
 
-    def test_enable_disable_questions():
-        # tests if questions are enabled / disabled
-        question = Question("4", "Is Python a programming language?", "Yes")
-        question.disable()
-        assert not question.active, "Question should be disabled."
-        question.enable()
-        assert question.active, "Question should be ensabled."
-        print("Enable/disable test passed!")
+def test_check_incorrect_answer():
+    question = Question("2", "What is the capital of France?", "Paris")
+    assert not question.check_answer("Lyon"), "Should return False for incorrect answer."
+
+
+def test_is_correct_case_insensitive():
+    question = Question("3", "What is the capital of France?", "Paris")
+    assert question.check_answer("paris"), "The method should return True for a case-insensitive match."
+
+
+def test_enable_disable_questions():
+    # tests if questions are enabled / disabled
+    question = Question("4", "Is Python a programming language?", "Yes")
+    question.disable()
+    assert not question.active, "Question should be disabled."
+    question.enable()
+    assert question.active, "Question should be ensabled."
+    print("Enable/disable test passed!")
+
+
+def test_add_question(question_bank):
+    question = MagicMock()
+    question_bank.add_question(question)
+    assert question in question_bank.questions
 
 
 if __name__ == "__main__":
